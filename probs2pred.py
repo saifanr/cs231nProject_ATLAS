c2b194e8b61a2a2988fd6e9c24b33bdfc9c5fe9c
LAPPY\Jay <jdedhia@stanford.edu>
2018-06-05 20:07:28 -0700

    made changes to probs2pred - it works

--- probs2pred.py
+++ probs2pred.py
@@ -7,14 +7,15 @@ if __name__ == '__main__':
         raise Exception("Need at least the input directory")
     input_dir = sys.argv[1]
 
-    f = open(os.path.join( input_path, 'dice_scores.txt' ), 'w')
-
     all_paths = []
     for dirpath, dirnames, files in os.walk(input_dir):
         if os.path.basename(dirpath)[0:3] == 't01':
             all_paths.append(dirpath)
 
+    count = 0
     for path in all_paths:
-        probs = np.load(os.path.join(input_path, 'probs.npy'))
-        pred = np.argmax(result, 3).astype(np.float32)
+        count+=1
+        probs = np.load(os.path.join(path, 'probs.npy'))
+        pred = np.argmax(probs, 3).astype(np.float32)
+        print('Saving image no.', count)
         np.save(os.path.join(path, 'preds'), pred)
